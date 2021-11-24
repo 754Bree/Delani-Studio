5d52febfcf80172c09ef85a6859f60e314d14b05
Briana <briana.odhiambo@student.moringaschool.com>
2021-11-24 22:24:58 +0300

    Add service background Image

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*$ (document).ready(function() {
+
+    //What we do:
+    $('.des').click(){
+        $('.text-1').toggle();
+        $('.des1').toggle();
+    });
+
+    $('.dev').click(function(){
+        $('.text-2').toggle();
+        $('.dev1').toggle();
+    });
+
+    $('.prod').click(function () {
+        $('.text-3').toggle();
+        $('.prod1').toggle();
+    });
+
+
+});*/
