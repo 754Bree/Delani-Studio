73eefd70c791091a39aa16d8da86a920f89c444e
Briana <briana.odhiambo@student.moringaschool.com>
2021-12-06 15:57:34 +0300

    Add changes to js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,111 @@
-function myFunction() {
+$(document).ready(function () {
+
+    // toggle
+    $('#design').click(function () {
+        $(this).hide();
+        $('.design').show();
+    });
+    $('.design').click(function () {
+        $(this).hide();
+        $('#design').show();
+    });
+    $('#dev').click(function () {
+        $(this).hide();
+        $('.dev').toggle();
+    });
+    $('.dev').click(function () {
+        $(this).hide();
+        $('#dev').toggle();
+    });
+    $('#product').click(function () {
+        $(this).hide();
+        $('.product').toggle();
+    });
+    $('.product').click(function () {
+        $(this).hide();
+        $('#product').toggle();
+    });
+
+    // overlays
+    $(".one").mouseover(function () {
+        $("#one").show();
+    });
+    $(".one").mouseout(function () {
+        $("#one").hide();
+    });
+
+    $(".two").mouseover(function () {
+        $("#two").show();
+    });
+    $(".two").mouseout(function () {
+        $("#two").hide();
+    });
+    $(".three").mouseover(function () {
+        $("#three").show();
+    });
+    $(".three").mouseout(function () {
+        $("#three").hide();
+    });
+    $(".four").mouseover(function () {
+        $("#four").show();
+    });
+    $(".four").mouseout(function () {
+        $("#four").hide();
+    });
+    $(".five").mouseover(function () {
+        $("#five").show();
+    });
+    $(".five").mouseout(function () {
+        $("#five").hide();
+    });
+    $(".six").mouseover(function () {
+        $("#six").show();
+    });
+    $(".six").mouseout(function () {
+        $("#six").hide();
+    });
+    $(".seven").mouseover(function () {
+        $("#seven").show();
+    });
+    $(".seven").mouseout(function () {
+        $("#seven").hide();
+    });
+    $(".eight").mouseover(function () {
+        $("#eight").show();
+    });
+    $(".eight").mouseout(function () {
+        $("#eight").hide();
+    });
+
+    //submit message
+    $("form#feedbackform").submit(function (event) {
+        event.preventDefault();
+        var name = $("#name").val();
+        alert("Dear " + name + " we have received your message. Thank you for reaching out to Delani Studio");
+        document.getElementById("feedbackform").reset();
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function myFunction() {
     var x = document.getElementById("myText");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
-}
+}*/
 
 
 
