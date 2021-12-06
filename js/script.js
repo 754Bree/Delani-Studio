105239b399db9e7e6f1b4ba5eabffcecd50d6ee8
Briana <briana.odhiambo@student.moringaschool.com>
2021-12-06 15:52:13 +0300

    Add changes to entire document

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+function myFunction() {
+    var x = document.getElementById("myText");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+
 /*$ (document).ready(function() {
 
     //What we do:
